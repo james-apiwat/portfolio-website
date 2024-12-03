b1b06ee59e9f3e363d7be671076b218d1bd16466
james-apiwat <122668930+james-apiwat@users.noreply.github.com>
2024-12-04 02:27:21 +0700

    add website files

---
+++ app.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector(".nav")
+window.addEventListener("scroll", stickyNavbar)
+
+function stickyNavbar() {
+    if(window.scrollY>nav.offsetHeight+20) {
+        nav.classList.add("active")
+    } else {
+        nav.classList.remove("active")
+    }
+}
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
---
+++ resume/README.txt
@@ -0,0 +1,2 @@
+# Portfolio-Website
+Building Portfolio Website using HTML, CSS and JavaScript.
